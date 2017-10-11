cd859a2a3013d17e4f24c6cdc3fb467bfc93c254
Joe Buhlig <hello@joebuhlig.com>
2017-10-11 09:45:08 -0500

    Initial commit

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: discourse-envato-customizations
+# about: Adds tweaks to the Envato Discourse forums.
+# version: 0.1
+# author: Joe Buhlig joebuhlig.com
+# url: https://www.github.com/joebuhlig/discourse-envato-customizations
+
+enabled_site_setting :envato_customizations_enabled
